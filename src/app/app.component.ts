e8266ef32d46f1b5f84062675db541595d46bb0e
Dennis <kamponsah.amponsah@gmail.com>
2023-10-01 16:54:20 +0100

    added custom theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+
+  changeTheme(theme: string) {
+    const img: Element | null = document.querySelector('app-root');
+    img?.setAttribute('class', theme);
+  }
 }
